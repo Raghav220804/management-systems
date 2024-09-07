70a6d3f09ef842e916dfb482746bfd590ff81e9d
Raghav Bohra <113873570+Raghav220804@users.noreply.github.com>
2024-09-08 00:17:15 +0530

    Update student management.py

--- student management.py
+++ student management.py
@@ -1,48 +1,68 @@
-print('Welcome to student management')
-menu()
+print('Welcome to Student Management')
+
 students = []
-student = {}
+
 def menu():
-    print('Enter an option of your choice')
-    print('1 Add students\n2 Delete students \n3 Find student \n4 Exit')
-    option = input('= ')
-    if option == '1':
-        add()
-    elif option == '2':
-        delstu()
-    elif option == '3':
-        findstu()
-    elif option == '4':
-        print('Good bye')
-    else:
-        while option != '1' or option != '2' or option != '3':
-            print('Invalid entry, enter again')
-            option = input('= ')
+    while True:
+        print('\nEnter an option of your choice:')
+        print('1. Add students\n2. Delete students\n3. Find student\n4. List all students\n5. Exit')
+        option = input('= ')
+        if option == '1':
+            add()
+        elif option == '2':
+            delstu()
+        elif option == '3':
+            findstu()
+        elif option == '4':
+            list_students()
+        elif option == '5':
+            print('Goodbye!')
+            break
+        else:
+            print('Invalid entry, please enter a valid option.')
+
 def add():
-    num = int(input('how many students do you want to enroll?'))
-    for i in range (num):
-        student = {}
-        student['Name'] = input('Enter student Name: ' )
-        student['number'] = input('Enter roll number: ') 
-        student['quarter'] = input('Enter quarter: ')
-        students.append(student)
-    print('Number of students enrolled: ',len(students))
-    print('press any key to go back to menu')
-    goto_menu = input()
-    menu()
+    try:
+        num = int(input('How many students do you want to enroll? '))
+        for _ in range(num):
+            student = {}
+            student['Name'] = input('Enter student name: ')
+            student['number'] = input('Enter roll number: ')
+            students.append(student)
+        print('Number of students enrolled:', len(students))
+    except ValueError:
+        print('Please enter a valid number.')
+    input('Press Enter to go back to the menu.')
+
 def delstu():
-    delname = input('Enter the name of the student whose data you want to delete')
-    for student in students :
-        if student['Name'] == delname:
-            del student['Name']
-            del student['number']
-            del student['quarter']
-    goto_menu = input('Enter any key to go back')
-    menu()
-def findstu():
-    findname = input('Enter student name to get his data: ')
+    delnumber = input('Enter the roll number of the student whose data you want to delete: ')
     for student in students:
-        if student[Name] == findname:
-            print(student)
-    goto_menu = input('Enter any key to go back')
-    menu()
+        if student['number'] == delnumber:
+            students.remove(student)
+            print(f"Student with roll number {delnumber} has been removed.")
+            break
+    else:
+        print(f"No student found with the roll number {delnumber}.")
+    input('Press Enter to go back to the menu.')
+
+def findstu():
+    findnumber = input('Enter roll number to get the student data: ')
+    for student in students:
+        if student['number'] == findnumber:
+            print(f"Student found: Name: {student['Name']}, Roll Number: {student['number']}")
+            break
+    else:
+        print(f"No student found with the roll number {findnumber}.")
+    input('Press Enter to go back to the menu.')
+
+def list_students():
+    if not students:
+        print('No students enrolled yet.')
+    else:
+        print('\nList of all students:')
+        for idx, student in enumerate(students, start=1):
+            print(f"{idx}. Name: {student['Name']}, Roll Number: {student['number']}")
+    input('Press Enter to go back to the menu.')
+
+# Start the program by calling menu
+menu()
